43fda3ba9d384cd5a914f8b65f010fbd625d18d1
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-07-17 17:14:00 +0530

    Adding transforms

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -3,35 +3,65 @@ import * as THREE from 'three'
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
+
+
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Objects
- */
+
+
+// Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-/**
- * Sizes
- */
+// POSITION
+// mesh.position.normalize()
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+mesh.position.set(0.7, -0.6, 1)
+
+// SCALE
+mesh.scale.set(2, 0.5, 0.5)
+
+// ROTATION
+mesh.rotation.reorder('YXZ')
+mesh.rotation.x = Math.PI / 4
+mesh.rotation.y = Math.PI / 4
+
+console.log(mesh.position.length())                                     // Distance between center of scene and object
+console.log(mesh.position.distanceTo(new THREE.Vector3(0, 1, 2)))       // Distance between any point and object
+
+
+
+// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
 
-/**
- * Camera
- */
+
+
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
-/**
- * Renderer
- */
+camera.lookAt(mesh.position)
+
+console.log(mesh.position.distanceTo(camera.position))
+
+
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
+
+
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
