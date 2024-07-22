c9b470e79376621aebbd6f60a793a8dd43c883f2
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-07-22 16:41:32 +0530

    Added and optimised 300 random donuts

--- 12-3d-text/src/script.js
+++ 12-3d-text/src/script.js
@@ -24,6 +24,8 @@ scene.add(axesHelper)
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/5.png')
+matcapTexture.colorSpace = THREE.SRGBColorSpace
 
 // Fonts
 const fontLoader = new FontLoader()
@@ -53,10 +55,33 @@ fontLoader.load(
         // )
         textGeometry.center()
 
-        const textMaterial = new THREE.MeshBasicMaterial()
-        textMaterial.wireframe = true
-        const text = new THREE.Mesh(textGeometry, textMaterial)
+        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+        const text = new THREE.Mesh(textGeometry, material)
         scene.add(text)
+
+        console.time('donuts')
+
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+        // const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+
+        for (let i = 0; i < 300; i++) {
+            const donut = new THREE.Mesh(donutGeometry, material)
+
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+
+            const scale = Math.random()
+            donut.scale.x = scale
+            donut.scale.y = scale
+            donut.scale.z = scale
+
+            scene.add(donut)
+        }
+        console.timeEnd('donuts')
     }
 )
 
