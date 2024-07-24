fcba845eac6dae90b1557d4970b8c84dc58a9bd8
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-07-24 19:10:16 +0530

    added particles parallax

--- 19-scroll-based-animation/src/script.js
+++ 19-scroll-based-animation/src/script.js
@@ -12,6 +12,7 @@ const parameters = {
 
 gui.addColor(parameters, 'materialColor').onChange(() => {
     material.color.set(parameters.materialColor)
+    particlesMaterial.color.set(parameters.materialColor)
 })
 
 /**
@@ -68,6 +69,28 @@ torusKnot.position.y = - objectDistance * 2
 scene.add(torus, cone, torusKnot)
 
 const sectionMeshes = [torus, cone, torusKnot]
+
+/**
+ *  Particles
+ */
+const particlesCount = 200
+const positions = new Float32Array(particlesCount * 3)
+for (let i = 0; i < particlesCount; i++) {
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 6
+    positions[i * 3 + 1] = objectDistance * 0.5 - Math.random() * objectDistance * 3
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 6
+}
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+const particlesMaterial = new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    sizeAttenuation: true,
+    size: 0.03
+})
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
 
 /**
  *  Light
