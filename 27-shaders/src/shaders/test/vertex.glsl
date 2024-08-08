cc089de0424c686fd12e0f08b588648c97757e52
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-08 14:33:33 +0530

    vector variables

--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -5,5 +5,15 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 
 void main() {
+    // vec3 bar = vec3(1.0, 2.0, 3.0);
+    // bar.x = 2.0;
+    // bar.y = 3.0;
+    // bar.z = 4.0;
+
+    // vec3 color = vec3(1.0, 2.0, 3.0);
+    // color.r = 2.0;
+    // color.g = 3.0;
+    // color.b = 4.0;
+
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
