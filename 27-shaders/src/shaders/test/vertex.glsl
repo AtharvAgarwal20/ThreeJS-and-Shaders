c9ed3140421f2d61277f6097c6360be43eefd93c
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-08 22:28:31 +0530

    removed useless comments

--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -7,37 +7,7 @@ varying float vRandom;
 varying vec2 vUv;
 varying float vElevation;
 
-// float loremIpsum(float a, float b){
-//     // float a = 1.0;
-//     // float b = 3.0;
-
-//     return a + b;
-// }
-
 void main() {
-    // vec3 bar = vec3(1.0, 2.0, 3.0);
-    // bar.x = 2.0;
-    // bar.y = 3.0;
-    // bar.z = 4.0;
-
-    // vec3 color = vec3(1.0, 2.0, 3.0);
-    // color.r = 2.0;
-    // color.g = 3.0;
-    // color.b = 4.0;
-
-    // vec2 foo = vec2(1.0, 2.0);
-    // vec3 bar = vec3(foo, 3.0);
-    // vec2 demo = bar.xy;
-
-    // vec4 foo = vec4(1.0, 2.0, 3.0, 4.0);
-    // float barX = foo.x;
-    // float barY = foo.y;
-    // float barZ = foo.z;
-    // float barW = foo.w;                      // float barW = foo.a;
-
-    // float result = loremIpsum(1.0, 5.0);
-
-
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
     float elevation = sin(modelPosition.x * uFrequency.x - uTime) / 10.0;
