44a214d040cd620e2013fa893df23e52849b8993
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-16 19:55:50 +0530

    modifying existing threejs materials

--- 31-modified-materials/src/script.js
+++ 31-modified-materials/src/script.js
@@ -94,7 +94,7 @@ material.onBeforeCompile = (shader) => {
         '#include <begin_vertex>',
         `
             #include <begin_vertex>
-            float angle = (position.y + uTime) * 0.9;
+            float angle = sin(position.y + uTime) * 0.4;
             mat2 rotateMatrix = get2dRotateMatrix(angle);
 
             transformed.xz = rotateMatrix * transformed.xz;
@@ -124,7 +124,7 @@ depthMaterial.onBeforeCompile = (shader) => {
         `
             #include <beginnormal_vertex>
 
-            float angle = (position.y + uTime) * 0.9;
+            float angle = sin(position.y + uTime) * 0.4;
             mat2 rotateMatrix = get2dRotateMatrix(angle);
 
             objectNormal.xz = rotateMatrix * objectNormal.xz;
