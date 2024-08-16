99792e41734292ecb2a342a16ffa695a440e0aaf
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-16 14:16:24 +0530

    added waves

--- 29-raging-sea/src/script.js
+++ 29-raging-sea/src/script.js
@@ -25,8 +25,20 @@ const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
 // Material
 const waterMaterial = new THREE.ShaderMaterial({
     vertexShader: waterVertexShader,
-    fragmentShader: waterFragmentShader
+    fragmentShader: waterFragmentShader,
+    uniforms: {
+        uTime: { value: 0.0 },
+        uBigWavesElevation: { value: 0.2 },
+        uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+        uBigWavesSpeed: { value: 0.75 }
+    }
 })
+
+// Debug
+gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value').min(0).max(1).step(0.001).name("uBigWavesElevation")
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.01).name("uBigWavesFrequencyX")
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.01).name("uBigWavesFrequencyZ")
+gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value').min(0).max(4).step(0.001).name("uBigWavesSpeed")
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
@@ -83,6 +95,9 @@ const clock = new THREE.Clock()
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update water
+    waterMaterial.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
