bf0b3e6d18969595b78a5799641bde2fb028ff86
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-19 00:36:25 +0530

    added the rotate 2d function

--- 32-coffee-smoke-shader/src/shaders/coffeeSmoke/vertex.glsl
+++ 32-coffee-smoke-shader/src/shaders/coffeeSmoke/vertex.glsl
@@ -1,5 +1,13 @@
 varying vec2 vUv;
 
+vec2 rotate2D(vec2 value, float angle)
+{
+    float s = sin(angle);
+    float c = cos(angle);
+    mat2 m = mat2(c, s, -s, c);
+    return m * value;
+}
+
 void main(){
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 
