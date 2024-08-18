5df33e89d216b7a36bd0f5cfba32b1978cc53fcf
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-19 00:33:10 +0530

    fading out the edges

--- 32-coffee-smoke-shader/src/shaders/coffeeSmoke/fragment.glsl
+++ 32-coffee-smoke-shader/src/shaders/coffeeSmoke/fragment.glsl
@@ -13,6 +13,15 @@ void main(){
     // Smoke
     float smoke = texture(uPerlinTexture, smokeUv).r;
 
+    // Remap
+    smoke = smoothstep(0.4, 1.0, smoke);
+
+    // Edges
+    smoke *= smoothstep(0.0, 0.1, vUv.x);
+    smoke *= smoothstep(1.0, 0.9, vUv.x);
+    smoke *= smoothstep(0.0, 0.1, vUv.y);
+    smoke *= smoothstep(1.0, 0.4, vUv.y);
+
     gl_FragColor = vec4(1.0, 1.0, 1.0, smoke);
 
     #include <tonemapping_fragment>
