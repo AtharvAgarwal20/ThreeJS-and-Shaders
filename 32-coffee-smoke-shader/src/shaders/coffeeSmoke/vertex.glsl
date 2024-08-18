2ddca239e574bd9aaab53dbb1aeeaf995298a83e
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-19 00:53:59 +0530

    added wind effect with perlin texture

--- 32-coffee-smoke-shader/src/shaders/coffeeSmoke/vertex.glsl
+++ 32-coffee-smoke-shader/src/shaders/coffeeSmoke/vertex.glsl
@@ -19,6 +19,14 @@ void main(){
     float angle = twistPerlin * 10.0;
     newPosition.xz = rotate2D(newPosition.xz, angle);
 
+    // Wind
+    vec2 windOffset = vec2(
+        texture(uPerlinTexture, vec2(0.25, uTime * 0.01)).r - 0.5,
+        texture(uPerlinTexture, vec2(0.75, uTime * 0.01)).r - 0.5
+    );
+    windOffset *= pow(uv.y, 2.0) * 10.0;
+    newPosition.xz += windOffset;
+
     gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
 
     vUv = uv;
