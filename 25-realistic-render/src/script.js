cd750876a5ccc8ed11ba83be15414639c04789f1
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-12-18 14:53:24 +0530

    feat: shadows

--- 25-realistic-render/src/script.js
+++ 25-realistic-render/src/script.js
@@ -29,6 +29,8 @@ const updateAllMaterials = () => {
   scene.traverse((child) => {
     if (child.isMesh) {
       // Activate shadow here
+      child.castShadow = true;
+      child.receiveShadow = true;
     }
   });
 };
@@ -47,6 +49,47 @@ rgbeLoader.load("/environmentMaps/0/2k.hdr", (environmentMap) => {
   scene.background = environmentMap;
   scene.environment = environmentMap;
 });
+
+/**
+ * Directional Light
+ */
+
+const directionalLight = new THREE.DirectionalLight("#ffffff", 6);
+directionalLight.position.set(-4, 6.5, 2.5);
+scene.add(directionalLight);
+
+gui
+  .add(directionalLight, "intensity")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("Light Intensity");
+
+gui
+  .add(directionalLight.position, "x", -20, 20, 0.001)
+  .name("light position x");
+gui
+  .add(directionalLight.position, "y", -20, 20, 0.001)
+  .name("light position y");
+gui
+  .add(directionalLight.position, "z", -20, 20, 0.001)
+  .name("light position z");
+
+// Shadows
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.mapSize.set(1024, 1024);
+gui.add(directionalLight, "castShadow");
+
+// Helper
+// const directionalLightCameraHelper = new THREE.CameraHelper(
+//   directionalLight.shadow.camera
+// );
+// scene.add(directionalLightCameraHelper);
+
+// Target
+directionalLight.target.position.set(0, 4, 0);
+directionalLight.target.updateWorldMatrix();
 
 /**
  * Models
@@ -125,6 +168,10 @@ gui.add(renderer, "toneMapping", {
 });
 
 gui.add(renderer, "toneMappingExposure").min(0).max(10).step(0.001);
+
+// Shadows
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 /**
  * Animate
