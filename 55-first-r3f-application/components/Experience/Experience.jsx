2738e6a5fdd2cfa86aa0737319bf6676d75afbe1
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-12-19 18:04:50 +0530

    Moving the mesh

--- 55-first-r3f-application/components/Experience/Experience.jsx
+++ 55-first-r3f-application/components/Experience/Experience.jsx
@@ -1,9 +1,14 @@
 export default function Experience() {
     return (
         <>
-            <mesh>
-                <sphereGeometry />
-                <meshBasicMaterial />
+            <mesh
+                scale={1.5}
+                position={[2, 0, 0]}
+                rotation={[Math.PI / 4, 0, 0]}
+            >
+                {/*<sphereGeometry args={[1.5, 32, 32]} />*/}
+                <boxGeometry />
+                <meshBasicMaterial color="red" wireframe={true} />
             </mesh>
         </>
     );
