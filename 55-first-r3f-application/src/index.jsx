a7533fac7da0db8c2763f791c6204e4415d98a6f
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-12-19 17:29:34 +0530

    feat: added a torus knot

--- 55-first-r3f-application/src/index.jsx
+++ 55-first-r3f-application/src/index.jsx
@@ -1,6 +1,7 @@
+import App from "./App";
 import "./style.css";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-root.render(<>Soon to be a badass R3F application</>);
+root.render(<App />);
