18f4ce0a47ef65eacc2f650ecbf8a3b7f52c15eb
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-12-20 22:50:55 +0530

    Position and color debug on leva

--- 57-debug-a-r3f-application/src/Experience.jsx
+++ 57-debug-a-r3f-application/src/Experience.jsx
@@ -1,15 +1,26 @@
 import {OrbitControls} from '@react-three/drei'
+import {useControls} from "leva";
 
 export default function Experience() {
+    const {spherePosition, sphereColor} = useControls({
+        spherePosition: {
+            value: {x: -2, y: 0, z: 0},
+            // min: -5,
+            // max: 5,
+            step: 0.01
+        },
+        sphereColor: '#ff0000'
+    })
+
     return <>
         <OrbitControls makeDefault/>
 
         <directionalLight position={[1, 2, 3]} intensity={4.5}/>
         <ambientLight intensity={1.5}/>
 
-        <mesh position-x={-2}>
+        <mesh position={[spherePosition.x, spherePosition.y, spherePosition.z]}>
             <sphereGeometry/>
-            <meshStandardMaterial color="orange"/>
+            <meshStandardMaterial color={sphereColor}/>
         </mesh>
 
         <mesh position-x={2} scale={1.5}>
