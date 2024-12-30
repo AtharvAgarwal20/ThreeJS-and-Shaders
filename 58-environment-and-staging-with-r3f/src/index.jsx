c6528cbf141b94508d164c6e81e8418c05941bd3
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-12-30 15:17:08 +0530

    Setting background colors with 3 different methods

--- 58-environment-and-staging-with-r3f/src/index.jsx
+++ 58-environment-and-staging-with-r3f/src/index.jsx
@@ -1,19 +1,24 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
+import {Canvas} from '@react-three/fiber'
 import Experience from './Experience.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas
-        camera={ {
+        camera={{
             fov: 45,
             near: 0.1,
             far: 200,
-            position: [ - 4, 3, 6 ]
-        } }
+            position: [-4, 3, 6]
+        }}
+        // onCreated={({gl, scene}) => {
+        //     // gl.setClearColor("#ff0000", 1)
+        //     scene.background = new THREE.Color('#ff0000')
+        // }}
     >
-        <Experience />
+        <color args={["#ff0000"]} attach="background"/>
+        <Experience/>
     </Canvas>
 )
