5583c47a862998d1bf0ee40f7584158944ee3b3d
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-12-31 16:57:41 +0530

    Preload models

--- 59-load-models-with-r3f/src/Model.jsx
+++ 59-load-models-with-r3f/src/Model.jsx
@@ -4,3 +4,5 @@ export default function Model() {
     const model = useGLTF('./hamburger.glb')
     return <primitive object={model.scene} scale={0.35} position={[0, -1, 0]}/>;
 }
+
+useGLTF.preload('./hamburger.glb');
