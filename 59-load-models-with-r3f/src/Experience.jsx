f02f5fc9105311111756696cb56a671b417264d2
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-12-31 12:34:02 +0530

    Using React suspense to lazy load models

--- 59-load-models-with-r3f/src/Experience.jsx
+++ 59-load-models-with-r3f/src/Experience.jsx
@@ -1,15 +1,10 @@
 import {OrbitControls} from '@react-three/drei'
 import {Perf} from 'r3f-perf'
-import {useLoader} from "@react-three/fiber";
-import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
-import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader.js";
+import Model from "./Model.jsx";
+import {Suspense} from "react";
 
 export default function Experience() {
-    const model = useLoader(GLTFLoader, './hamburger-draco.glb', (loader) => {
-        const dracoLoader = new DRACOLoader()
-        dracoLoader.setDecoderPath('./draco/')
-        loader.setDRACOLoader(dracoLoader)
-    })
+
     return <>
         <Perf position="top-left"/>
 
@@ -23,6 +18,8 @@ export default function Experience() {
             <meshStandardMaterial color="greenyellow"/>
         </mesh>
 
-        <primitive object={model.scene} scale={0.35}/>
+        <Suspense>
+            <Model/>
+        </Suspense>
     </>
 }
