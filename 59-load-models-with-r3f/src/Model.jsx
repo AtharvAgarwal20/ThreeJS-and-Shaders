667f9ff9e82aacff5d6b1ea86047ec3680c04f63
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-12-31 17:58:13 +0530

    Multiple models using the same glb file can be added using the Clone helper

--- 59-load-models-with-r3f/src/Model.jsx
+++ 59-load-models-with-r3f/src/Model.jsx
@@ -1,8 +1,14 @@
-import {useGLTF} from "@react-three/drei";
+import {Clone, useGLTF} from "@react-three/drei";
 
 export default function Model() {
     const model = useGLTF('./hamburger.glb')
-    return <primitive object={model.scene} scale={0.35} position={[0, -1, 0]}/>;
+    return (
+        <>
+            <Clone object={model.scene} scale={0.35} position={[0, -1, 0]}/>
+            <Clone object={model.scene} scale={0.35} position={[4, -1, 0]}/>
+            <Clone object={model.scene} scale={0.35} position={[-4, -1, 0]}/>
+        </>
+    );
 }
 
 useGLTF.preload('./hamburger.glb');
